7ad27701999b4388c4649dc4e1019e18a22067d9
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2020-06-11 22:12:20 +0200

    editor works

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../scss/main.scss';
 
+
 const entry = localStorage.getItem('entry');
 let result = '';
 
@@ -8,12 +9,30 @@ if (entry) {
     result = entry;
 }
 
-const inputEntry = document.querySelector('.form__textarea--js');
+
+const inputEntry = document.querySelector('.form__textarea--js');  //pobranie tekstu
 inputEntry.value = result;
 
-const buttonSave = document.querySelector('.button__save--js');
 
 
-buttonSave.addEventListener('click', () => {
+const buttonSave = document.querySelector('.button__save--js');     //przpięcie button save
+
+buttonSave.addEventListener('click', () => {                        //na klik zapisz do localStorage
     localStorage.setItem('entry', inputEntry.value);
+});
+
+
+
+const buttonLoad = document.querySelector('.button__load--js');     //przypięcie button load
+
+buttonLoad.addEventListener('click', () => {
+    localStorage.getItem('entry', inputEntry.value);                //na klik załaduj z localStorage
+});
+
+
+
+const buttonClear = document.querySelector('.button__clear--js');     //przypięcie button clear
+
+buttonClear.addEventListener('click', () => {
+    localStorage.removeItem('entry', inputEntry.value);                //wyczyszczenie localStorage
 });
